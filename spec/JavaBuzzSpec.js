8927ab350822afde72f6129be3372d5080fb8d56
John Dennehy <john.dennehy@gmail.com>
2019-10-14 16:20:01 +0100

    Amends formatting

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -1,28 +1,25 @@
 describe('Javabuzz', function() {
-  
   let javabuzz;
 
   beforeEach(function() {
     javabuzz = new Javabuzz();
   });
-  
+
   describe('when playing, says', function() {
     it('"Java" when a number is divisible by 3', function() {
-      expect(javabuzz.says(3)).toEqual("Java");
+      expect(javabuzz.says(3)).toEqual('Java');
     });
 
     it('"Buzz" when a number is divisible by 5', function() {
-      expect(javabuzz.says(5)).toEqual("Buzz");
+      expect(javabuzz.says(5)).toEqual('Buzz');
     });
 
     it('"JavaBuzz" when a number is divisible by 15', function() {
-      expect(javabuzz.says(15)).toEqual("JavaBuzz");
+      expect(javabuzz.says(15)).toEqual('JavaBuzz');
     });
 
     it('the original number if it is not divisible by 3 or 5', function() {
       expect(javabuzz.says(4)).toEqual(4);
     });
-
-  })
-
+  });
 });
--- src/JavaBuzz.js
+++ src/JavaBuzz.js
@@ -1,23 +1,22 @@
 var Javabuzz = function() {};
 
 Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
-  return (number % divisor === 0);
+  return number % divisor === 0;
 };
 
 Javabuzz.prototype.says = function(number) {
-  output = ""
+  output = '';
 
-  if(this._isDivisibleBy(number,3)) {
-    output += "Java"
+  if (this._isDivisibleBy(number, 3)) {
+    output += 'Java';
   }
-  if(this._isDivisibleBy(number,5)) {
-    output += "Buzz"
-  }
-  
-  if(output === "") {
-    return number
+  if (this._isDivisibleBy(number, 5)) {
+    output += 'Buzz';
   }
 
-  return output
+  if (output === '') {
+    return number;
+  }
 
+  return output;
 };
