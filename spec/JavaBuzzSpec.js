85906882d07809ecfb4c4bc402261bb5ce161cf0
John Dennehy <john.dennehy@gmail.com>
2019-10-14 16:17:26 +0100

    Adds fizzbuzz created using JS

---
+++ spec/JavaBuzzSpec.js
@@ -0,0 +1,28 @@
+describe('Javabuzz', function() {
+  
+  let javabuzz;
+
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+  
+  describe('when playing, says', function() {
+    it('"Java" when a number is divisible by 3', function() {
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
+
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(javabuzz.says(5)).toEqual("Buzz");
+    });
+
+    it('"JavaBuzz" when a number is divisible by 15', function() {
+      expect(javabuzz.says(15)).toEqual("JavaBuzz");
+    });
+
+    it('the original number if it is not divisible by 3 or 5', function() {
+      expect(javabuzz.says(4)).toEqual(4);
+    });
+
+  })
+
+});
---
+++ src/JavaBuzz.js
@@ -0,0 +1,23 @@
+var Javabuzz = function() {};
+
+Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
+};
+
+Javabuzz.prototype.says = function(number) {
+  output = ""
+
+  if(this._isDivisibleBy(number,3)) {
+    output += "Java"
+  }
+  if(this._isDivisibleBy(number,5)) {
+    output += "Buzz"
+  }
+  
+  if(output === "") {
+    return number
+  }
+
+  return output
+
+};
